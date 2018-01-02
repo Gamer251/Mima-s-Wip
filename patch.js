1d76e814a382f4ed2908d8c7532e7811761f079e
IkuTronHD <34961368+IkuTronHD@users.noreply.github.com>
2018-01-02 11:47:54 +0100

    Add files via upload

---
+++ patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"nmlgc/base_tasofro"
+	],
+	"id": "mima",
+	"servers": [
+		"https://raw.githubusercontent.com/Gamer251/thcrap-gamer251/master/mima/"
+	],
+	"title": "The return of Mima! (English)"
+}
